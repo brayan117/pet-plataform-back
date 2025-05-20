701853de01fbb62cdfd7977390f5abd82319dc96
brayan117 <duranbdj12@gmail.com>
2025-05-20 12:25:24 -0500

    correcciones de versiones python compatibles

--- startup.sh
+++ startup.sh
@@ -5,11 +5,10 @@ echo "Iniciando la aplicación Flask..."
 # Asegurarse de que el puerto esté configurado, si no, usar el puerto 8000 por defecto
 PORT=${PORT:-8000}
 
-# Instalar dependencias si es necesario
-if [ -f "requirements.txt" ]; then
-    echo "Instalando dependencias..."
-    pip install -r requirements.txt
-fi
+# Instalar dependencias
+echo "Instalando dependencias..."
+pip install --no-cache-dir -r requirements.txt
 
 # Iniciar la aplicación con Waitress
+echo "Iniciando la aplicación en el puerto $PORT..."
 waitress-serve --host=0.0.0.0 --port=$PORT --ident=PetPlatformBackend app:create_app
